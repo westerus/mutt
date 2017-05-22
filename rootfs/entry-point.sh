9f9eb292f3ce8f01474d1bc3b513dd49cda95f06
Ernesto Pérez <westerus@gmail.com>
2017-05-22 15:51:51 +0200

    Fix entrypoint
    Modify entry-point command

--- rootfs/entry-point.sh
+++ rootfs/entry-point.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -e
-## COMMANDS... ##
+
+for file in /usr/local/share/ca-certificates/*.crt
+do
+  if [ -e "$file" ]
+  then
+    echo "There are certificates files"
+    /usr/sbin/update-ca-certificates
+  fi
+done
+
 exec "$@"
